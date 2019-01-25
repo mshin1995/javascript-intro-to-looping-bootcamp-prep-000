ad359eea73781746de1e9d3b9e9ec38720946cda
Matthew Shin <mshin1995@gmail.com>
2019-01-25 05:14:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,14 +16,9 @@ var i = 0;
 function incrementVariable() {
   i = i + 1;
 }
- 
-do {
-  console.log("doo-bee-doo-bee-doo");
-  incrementVariable();
-} while (i < 5);
 function doWhileLoop(array) {
   do {
     
-  } while ()
+  } while (array.length > 0 && incrementVariable())
   return array
 }
